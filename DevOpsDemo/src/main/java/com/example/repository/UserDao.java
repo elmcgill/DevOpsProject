be8b97b199d2b73396af7c245a11d15353461886
Ethan McGill <37718257+elmcgill@users.noreply.github.com>
2021-06-28 20:33:20 -0500

    tweaked the spring project again

--- DevOpsDemo/src/main/java/com/example/repository/UserDao.java
+++ DevOpsDemo/src/main/java/com/example/repository/UserDao.java
@@ -2,9 +2,17 @@ package com.example.repository;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.example.model.User;
 
 public class UserDao {
+	
+	@Autowired
+	public UserDao() {
+		
+	}
 	
 	public List<User> getAllUsers(){
 		List<User> users = new ArrayList<User>();
